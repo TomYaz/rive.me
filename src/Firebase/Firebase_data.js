c934bee5156a5e37e528abc61db61151d0434719
Thomas Yaz <thomas.yazitzoglou@gmail.com>
2025-09-18 00:45:33 +0300

    updated gitignore

--- src/Firebase/Firebase_data.js
+++ src/Firebase/Firebase_data.js
@@ -1,1 +1,20 @@
+// this file must never make it onto git!
+// if you're not sure please let someone know, but for the love of god don't push these keys to prod
 
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyAvcljCyxht6NMNtb7WeVSxtQ00KXN1l9o",
+
+  authDomain: "rive-683a9.firebaseapp.com",
+
+  projectId: "rive-683a9",
+
+  storageBucket: "rive-683a9.firebasestorage.app",
+
+  messagingSenderId: "654082631864",
+
+  appId: "1:654082631864:web:3244cd1ca5229a225fec4d",
+
+  measurementId: "G-2QXSKGRG5T"
+
+};
