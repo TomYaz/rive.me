906b1ea34c227b1d84ead571fcca3edec26a1441
Thomas Yaz <thomas.yazitzoglou@gmail.com>
2025-09-18 00:35:58 +0300

    firebase config added

---
+++ src/Firebase/Firebase_data.js
@@ -0,0 +1,12 @@
+/* Make sure this file never makes it on github */
+
+// rive Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAvcljCyxht6NMNtb7WeVSxtQ00KXN1l9o",
+    authDomain: "rive-683a9.firebaseapp.com",
+    projectId: "rive-683a9",
+    storageBucket: "rive-683a9.firebasestorage.app",
+    messagingSenderId: "654082631864",
+    appId: "1:654082631864:web:3244cd1ca5229a225fec4d",
+    measurementId: "G-2QXSKGRG5T"
+};
